75237d1e1f2b75e280c96233ec991ecc8bc0b9f5
tenchipe23 <josemanueltenchipe@gmail.com>
2024-10-10 00:43:25 -0600

    fead: Se agrega el archivo index.js al microservicio

---
+++ API_LA_DELICIA/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./src/routes/authRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
