2692a4756c3c248a384e953f593c310900557055
karl262 <carlins502@gmail.com>
2024-10-09 20:31:34 -0600

    feat: agregar archivo db.js a carpeta config

---
+++ API_LA_DELICIA/Products/src/config/db.js
@@ -0,0 +1,19 @@
+const {Pool} = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,  // 'product-db'
+    user: process.env.PRODUCT_DB_USER,  // 'user'
+    password: process.env.PRODUCT_DB_PASSWORD,  // 'password'
+    database: process.env.PRODUCT_DB_NAME,  // 'product_db'
+    port: process.env.DB_PORT || 5432  // Asegúrate de que este puerto sea 5432
+});
+
+console.log('Database connection config:', {
+    host: process.env.DB_HOST,
+    user: process.env.PRODUCT_DB_USER,
+    database: process.env.PRODUCT_DB_NAME,
+    port: process.env.DB_PORT || 5432
+});
+
+module.exports=pool;
