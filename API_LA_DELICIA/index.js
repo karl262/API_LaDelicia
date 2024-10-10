9f2f8ad25cd49730eb6ad3649d36991eb578909b
Roberto-H-Z <20233l001058@gmail.com>
2024-10-09 21:19:26 -0600

    fead: Se agrega el archivo index

---
+++ API_LA_DELICIA/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const saleRoutes = require('./src/routes/saleRoutes');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', saleRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
