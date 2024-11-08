1d3866fa0d36cbc934724c6b4d756d902a6997a1
Gael Alejandro Cordova Diaz <gaelalejandrocordovadiaz@gmail.com>
2024-11-07 19:39:56 -0600

    fix: refactorizacion en el archivo productsModel.js de products

--- API_LA_DELICIA/Products/src/models/productsModel.js
+++ API_LA_DELICIA/Products/src/models/productsModel.js
@@ -1,4 +1,4 @@
-const pool = require('../config/db');
+import { pool } from '../config/db';
 
 class Product {
 
@@ -59,4 +59,4 @@ class Product {
     }
 }
 
-module.exports = Product;
+export default Product;
