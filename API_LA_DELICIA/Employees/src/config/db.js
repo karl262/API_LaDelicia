4027a702a6c479a662344ee9f68781a126197719
Gael Alejandro Cordova Diaz <gaelalejandrocordovadiaz@gmail.com>
2024-11-11 18:33:41 -0600

    fix: refactorizar el archivo db.js del microservicio Employees

--- API_LA_DELICIA/Employees/src/config/db.js
+++ API_LA_DELICIA/Employees/src/config/db.js
@@ -1,19 +1,22 @@
-import { Pool } from 'pg';
-import {dotenv} from 'dotenv';
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
 dotenv.config();
 
+const { Pool } = pkg;
+
 const pool = new Pool({
-    host:process.env.DB_HOST,  // 'employee-db'
-    user:process.env.EMPLOYEE_DB_USER,  // 'user'
+    host: process.env.DB_HOST,  // 'employee-db'
+    user: process.env.EMPLOYEE_DB_USER,  // 'user'
     password: process.env.EMPLOYEE_DB_PASSWORD,  // 'password'
-    database:process.env.EMPLOYEE_DB_NAME,  // 'employee_db'
+    database: process.env.EMPLOYEE_DB_NAME,  // 'employee_db'
     port: process.env.DB_PORT || 5432  // Asegúrate de que este puerto sea 5432
 });
 
 console.log('Database connection config:', {
-    host:process.env.DB_HOST,
-    user:process.env.EMPLOYEE_DB_USER,
-    database:process.env.EMPLOYEE_DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.EMPLOYEE_DB_USER,
+    database: process.env.EMPLOYEE_DB_NAME,
     port: process.env.DB_PORT || 5432
 });
 
