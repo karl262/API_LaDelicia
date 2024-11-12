ce03047c6ac31c9d8b94ed3885df444d03a3f81b
Roberto-H-Z <20233l001058@gmail.com>
2024-11-12 13:42:34 -0600

    fix:Modifica rutas en orderRoutes.js

--- API_LA_DELICIA/Orders/src/routes/orderRoutes.js
+++ API_LA_DELICIA/Orders/src/routes/orderRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { validateToken } = require('../middleware/authMiddleware');
 const OrderController = require('../controllers/orderController');
 
-router.post( '/convert-to-sale/:orderId', validateToken,OrderController.convertOrderToSale);
+// Ya no se requiere el token aquí
+router.post('/orders', OrderController.createOrder);   // Crear un pedido
+router.get('/orders', OrderController.getAllOrders);  // Obtener todos los pedidos
+router.get('/orders/:id', OrderController.getOrderById); // Obtener un pedido por ID
+router.delete('/orders/:id', OrderController.deleteOrder);  // Eliminar un pedido
 
 module.exports = router;
