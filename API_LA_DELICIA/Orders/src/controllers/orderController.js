0c73286e91d6784b5a5202d3f9402aba9e97a5d5
Roberto-H-Z <20233l001058@gmail.com>
2024-11-12 13:41:36 -0600

    fix:Ajusta lógica en orderController

--- API_LA_DELICIA/Orders/src/controllers/orderController.js
+++ API_LA_DELICIA/Orders/src/controllers/orderController.js
@@ -1,7 +1,9 @@
-const User = require('../models/usersMoldel');
-const { body, validationResult } = require('express-validator');
+const OrderModel = require('../models/orderModels')
+
 
 class OrderController {
+
+  // Convertir una orden a una venta
     static async convertOrderToSale(req, res) {
       const { orderId } = req.params;
       const pool = require('../config/database');
@@ -56,6 +58,59 @@ class OrderController {
         client.release();
       }
     }
+
+
+    
+// Crear un nuevo pedido
+        static async createOrder(req, res) {
+          try {
+              const order = await Order.create(req.body);
+              res.status(201).json(order);
+          } catch (error) {
+              res.status(500).json({ error: error.message });
+          }
+      }
+  
+  // Obtener todos los pedidos
+  static async getAllOrders(req, res) {
+    try {
+      const orders = await OrderModel.findAll(); // Usar el modelo correcto
+      res.status(200).json(orders);
+    } catch (error) {
+      console.error('Error al obtener todas las órdenes:', error.message);
+      res.status(500).json({ error: error.message });
+    }
+  }
+      
+  
+      // Obtener un pedido por ID
+      static async getOrderById(req, res) {
+        try {
+          const order = await OrderModel.getOrderById(req.params.id);
+          if (!order) {
+            return res.status(404).json({ message: 'Pedido no encontrado' });
+          }
+          res.status(200).json(order);
+        } catch (error) {
+          res.status(500).json({ error: error.message });
+        }
+      }
+  
+  
+      // Eliminar un pedido
+      static async deleteOrder(req, res) {
+          try {
+              const order = await Order.delete(req.params.id);
+              if (!order) {
+                  return res.status(404).json({ message: 'Pedido no encontrado' });
+              }
+              res.status(200).json({ message: 'Pedido eliminado con éxito' });
+          } catch (error) {
+              res.status(500).json({ error: error.message });
+          }
+      }
+
+
   }
 
 module.exports = OrderController;
