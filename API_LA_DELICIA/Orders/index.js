7b917387d0a0c7663cf04898ad38574e57dd6759
Roberto-H-Z <20233l001058@gmail.com>
2024-11-12 13:40:28 -0600

    fix:Corrige manejo de rutas en index.js

--- API_LA_DELICIA/Orders/index.js
+++ API_LA_DELICIA/Orders/index.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require('express'); 
 const cors = require('cors');
-const usersRoutes = require('./src/routes/orderRoutes');
+const dotenv = require('dotenv');
+const ordersRoutes = require('./src/routes/orderRoutes'); // Ajusta esta ruta si es necesario
 
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-app.use('/api', usersRoutes);
+
+// Usa la ruta '/api/orders' para todas las rutas de pedidos
+app.use('/api', ordersRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
