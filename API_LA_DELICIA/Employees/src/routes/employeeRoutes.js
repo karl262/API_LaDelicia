d4fbf1a28999140abec93db1b119f4382755c714
karl262 <carlins502@gmail.com>
2024-11-15 08:53:16 -0600

    fix: Modificar archivos de rutas

--- API_LA_DELICIA/Employees/src/routes/employeeRoutes.js
+++ API_LA_DELICIA/Employees/src/routes/employeeRoutes.js
@@ -2,9 +2,9 @@ import express from 'express';
 import EmployeeController from '../controllers/employeeController.js';  
 import authMiddleware from '../middlewares/auth.js';  
 
-export const router = express.Router();
+ const router = express.Router();
 
-router.use(authMiddleware);
+//router.use(authMiddleware);
 
 router.get('/get/employees', EmployeeController.getAllEmployees);
 router.get('/get/employees/by/id/:id', EmployeeController.getEmployeeById);
@@ -12,10 +12,5 @@ router.get('/get/employees/by/name/:name', EmployeeController.getEmployeeByName)
 router.post('/create/employee', EmployeeController.createEmployee);
 router.put('/update/employee/:id', EmployeeController.updateEmployee);
 router.delete('/delete/employees/:id', EmployeeController.deleteEmployee);
-
-
-router.get('/health', (req, res) => {
-    res.status(200).json({ status: 'OK', service: 'employee-service' });
-});
 
 export default router;
