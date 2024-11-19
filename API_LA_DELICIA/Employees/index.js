aa8da7bf95687dc6c26680871dd8f957cf85a791
tenchipe23 <josemanueltenchipe@gmail.com>
2024-11-19 17:37:48 -0600

    "feat: agregar cors para a los microservicios faltantes y correcciones en algunos codigos"

--- API_LA_DELICIA/Employees/index.js
+++ API_LA_DELICIA/Employees/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import employeesRoutes from './src/routes/employeeRoutes.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
+
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api/employees', employeesRoutes);
 
