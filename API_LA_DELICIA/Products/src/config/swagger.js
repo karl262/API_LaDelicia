5c8c16ec3cab69b4150e91cb237c9f9142e62c68
Gael Alejandro Cordova Diaz <gaelalejandrocordovadiaz@gmail.com>
2024-11-26 12:09:14 -0600

    fix: modificar la descripcion del archivo swagger.js del microservicio de Products

--- API_LA_DELICIA/Products/src/config/swagger.js
+++ API_LA_DELICIA/Products/src/config/swagger.js
@@ -10,7 +10,19 @@ const options = {
     info: {
       title: 'Microservicio_Products',
       version: '1.0.0',
-      description: 'Documentación de micriservicio de Products',
+      description: `
+      Este microservicio gestiona los productos en la plataforma **La Delicia**. Permite obtener, crear, actualizar y eliminar productos, además de realizar búsquedas de productos por diversos criterios como nombre, precio y cantidad en stock.
+
+      ### Funcionalidades principales:
+      - **Obtener todos los productos**
+      - **Obtener un producto por ID**
+      - **Obtener productos por nombre**
+      - **Obtener productos por precio**
+      - **Obtener productos por stock**
+      - **Crear un nuevo producto**
+      - **Actualizar un producto existente**
+      - **Eliminar un producto**
+    `,
     },
     servers: [
       {
